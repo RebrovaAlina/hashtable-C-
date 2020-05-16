536cdc12f3eadf968a8abd4762a45fbb0de6c17b
RebrovaAlina <64522511+RebrovaAlina@users.noreply.github.com>
2020-05-16 22:15:03 +0300

    Create hashtable.h

---
+++ hashtable.h
@@ -0,0 +1,40 @@
+#include <stdint.h>
+#include <string>
+#include <stdlib.h>
+#include<iostream>
+#define HASHTAB_SIZE 128 
+
+using namespace std;
+
+class hash_exp{
+        public:
+                int err;
+                std::string msg;
+};
+
+
+class hash1
+{
+        public:
+                enum hash_error{
+                        STERR_SUCCESS,
+                        STERR_FULL,
+                        STERR_DELETED,
+                        STERR_NOELEM,
+                        STERR_DELETEDALL ,
+                        STERR_NOADD
+                };
+                char *key;
+                int value;
+                hash1 *next;
+                hash1 *hashtab[HASHTAB_SIZE];
+
+                hash1();
+                void hashtab_add(char *key, int value);
+                void hash1_delete (char *key);
+                hash1 *lookup(char *key);
+                unsigned int hashtab_hash(char *key);
+                ~hash1();
+                char* resize_table(std::string key);
+
+};
