e7499fce004ee973807ea0cd70b3b50707f742eb
RebrovaAlina <64522511+RebrovaAlina@users.noreply.github.com>
2020-05-23 23:53:07 +0300

    Update utest.cpp

--- utest.cpp
+++ utest.cpp
@@ -15,53 +15,58 @@ int main() {
         cout << "Test_1\t->\tPASSED" << endl;
 
 
-
         try {
                 node->hashtab_add("Slon",190);
                 node->hashtab_add("Volk", 60);
-        } catch (...) {
-                cout << "Test_2\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_NOADD)
+                        cout << "Test_2\t->\tFAILED" << endl;
         }
         cout << "Test_2\t->\tPASSED" << endl;
 
+
         try {
                 node->lookup("Slon");
-        } catch (...) {
-                cout << "Test_3\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_NOELEM)
+                        cout << "Test_3\t->\tFAILED" << endl;
         }
         cout << "Test_3\t->\tPASSED" << endl;
 
 
         try {
                 node-> lookup("Crocodile");
-        } catch (...) {
-                cout << "Test_4\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_NOELEM)
+                        cout << "Test_4\t->\tFAILED" << endl;
         }
         cout << "Test_4\t->\tPASSED" << endl;
 
 
         try {
                 node->hash1_delete("Slon");
-        } catch (...) {
-                cout << "Test_5\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_DELETED)
+                        cout << "Test_5\t->\tFAILED" << endl;
         }
         cout << "Test_5\t->\tPASSED" << endl;
 
         try {
                 node->lookup("Slon");
-        } catch (...) {
-                cout << "Test_6\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_NOELEM)
+                        cout << "Test_6\t->\tFAILED" << endl;
         }
         cout << "Test_6\t->\tPASSED" << endl;
 
 
         try {
                 node->hash1_delete("Crocodile");
-        } catch (...) {
-                cout << "Test_7\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_DELETED)
+                        cout << "Test_7\t->\tFAILED" << endl;
         }
         cout << "Test_7\t->\tPASSED" << endl;
-
 
 
         try {
@@ -73,16 +78,18 @@ int main() {
                 node -> lookup(l);
                 node->hash1_delete(l);
                 node->lookup(l);
-        } catch (...) {
-                cout << "Test_8\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_NOELEM)
+                        cout << "Test_8\t->\tFAILED" << endl;
         }
         cout << "Test_8\t->\tPASSED" << endl;
 
 
         try {
                 delete node;
-        } catch (...) {
-                cout << "Test_9\t->\tFAILED" << endl;
+        } catch (const hash1<double>::hash_error &ex) {
+                if (ex != hash1<double>::STERR_DELETEDALL)
+                        cout << "Test_9\t->\tFAILED" << endl;
         }
         cout << "Test_9\t->\tPASSED" << endl;
 
